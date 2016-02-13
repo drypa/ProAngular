b78cae2f9b6410dfef0409cac5d71edcc6ebdfe8
DRypa <dr.drypa.k@mail.ru>
2016-02-13 20:53:46 +0300

    init(yeoman scaffold for angular project)

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marketApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the marketApp
+ */
+angular.module('marketApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
